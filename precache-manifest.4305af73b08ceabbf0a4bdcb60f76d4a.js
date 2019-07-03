1fb833b4a46c539ed812c98f97fe88bf90c2ea48
Brittney Postma <brittney.postma@hotmail.com>
2019-07-02 21:29:30 -0400

    Updates

---
+++ precache-manifest.4305af73b08ceabbf0a4bdcb60f76d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aa9d50869ec622f81165f80b8c2b97",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "3ec9ba9ea2ead3e08a85",
+    "url": "/Monsters-Rolodex/static/css/main.4d7811f1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3ec9ba9ea2ead3e08a85",
+    "url": "/Monsters-Rolodex/static/js/main.6839ee42.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  }
+]);
